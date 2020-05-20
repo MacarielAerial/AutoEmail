5fbbecc95ebfed65d4b1c96eeecf5bad085dcfd5
MacarielAerial <yyu.mam2020@london.edu>
2020-05-19 21:51:50 -0500

    bug ifx

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from distutils.core import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
   name = 'AutoEmail',
   packages = ['AutoEmail'],
-  version = '0.0.2',
+  version = '0.0.3',
   license='MIT',
   description = 'Send customised emails to a selected list of recipients',
+  long_description = long_description,
+  long_description_content_type="text/markdown",
   author = 'Yifei Yu',
   author_email = 'yyu.mam2020@london.edu',
   url = 'https://github.com/MacarielAerial/AutoEmail',
-  download_url = 'https://github.com/MacarielAerial/AutoEmail/archive/v_0.0.2.tar.gz',
+  download_url = 'https://github.com/MacarielAerial/AutoEmail/archive/v_0.0.3.tar.gz',
   keywords = ['AUTOMATION', 'OFFICE', 'MYSQL', 'EMAIL'],
   install_requires=[
           'numpy',
